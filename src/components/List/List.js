34da1820196069beb29f621a8b49c2fb67a61bc3
Mehul Warade <mehul.warade@bupa.com.au>
2022-02-14 18:23:58 +1100

    working edit

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,26 +4,20 @@ import { Btn } from "../Btn/Btn";
 
 export const List = ({ newtask }) => {
 
-  // const ischecked = [];
-
   const [tasks, setTasks] = useState([
-      "Task A", 
-      "Task B", 
-      "Task C"
+      ["Task A", false], 
+      ["Task B", false], 
+      ["Task C", true]
     ]);
-
-  // const [checked, setchecked] = useState([]);
-
-  // console.log(tasks);
 
   useEffect(() => {
     setTasks((previousState) => {
       if (newtask != null) {
-        //   for double same values
-        // if (previousState.includes(value)) {
-        //   return previousState;
-        // }
-        return [...previousState, newtask];
+        // for double same values
+        if (previousState.includes(newtask)) {
+          return previousState;
+        }
+        return [...previousState, [newtask, false]];
       }
       else{
           return previousState;
@@ -33,51 +27,31 @@ export const List = ({ newtask }) => {
 
   // Event + parameter: https://stackoverflow.com/a/42597619
   const clickhandler = (e, index) => {
-    // Not using toggle because we need to set whether they are checked or not.
+    console.log('task ' + index + ' is clicked');
     e.currentTarget.classList.toggle("checked");
-
-    // if(e.currentTarget.classList.value !== 'checked'){
-    //   e.currentTarget.classList.value = 'checked';
-    //   ischecked[index] = 1;
-    //   // console.log('unche');
-    //   setchecked((previousState) => {
-    //     return ischecked;
-    //   })
-      
-    // }
-    // else{
-    //   // console.log('chec');
-    //   e.currentTarget.classList.toggle("checked");
-    //   ischecked[index] = 0;
-    //   setchecked((previousState) => {
-    //     return ischecked;
-    //   })
-    // }
-    
-    
-
-    // console.log(e.currentTarget.classList.value);
-    // e.currentTarget.classList.toggle("checked");
-    // console.log(ischecked);
-    // console.log(checked);
   };
 
-  function close(itemIndex) {
+  function close(e, itemIndex) {
     //e.currentTarget.parentNode.style.display = 'none';
+    console.log('task ' + itemIndex + ' has been closed');
     setTasks(tasks.filter((item, index) => index !== itemIndex));
+    
+    // To stop the code from dropping further // Ref: https://stackoverflow.com/q/50829705
+    e.stopPropagation();
   }
 
   return (
     <ul id="list">
       {tasks.map((task, index) => (
-        <li onClick={(e) => clickhandler(e, index)} key={`${task}-${index}`}>
+        <li className = {task[1] ? 'checked' : ''} onClick={(e) => clickhandler(e, index)} key={`${task}-${index}`}>
           {task}
           <Btn
-            onClick={() => close(index)}
+            onClick={(e) => close(e, index)}
             className="xButton"
             input_text={"\u00D7"}
           />
         </li>
+        
       ))}
     </ul>
   );
