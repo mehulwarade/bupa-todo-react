b4608d502e9934d018ef484d1006ec1b6920c7f4
Mehul Warade <mehul.warade@bupa.com.au>
2022-02-14 20:12:19 +1100

    boolean works

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,12 +3,22 @@ import "./index.css";
 import { Btn } from "../Btn/Btn";
 
 export const List = ({ newtask }) => {
-
   const [tasks, setTasks] = useState([
-      ["Task A", false], 
-      ["Task B", false], 
-      ["Task C", true]
-    ]);
+    {
+      task: "Gym",
+      checked: false,
+    },
+    {
+      task: "Study",
+      checked: false,
+    },
+    {
+      task: "Job",
+      checked: false,
+    },
+  ]);
+
+  // console.log(tasks[0].task);
 
   useEffect(() => {
     setTasks((previousState) => {
@@ -17,42 +27,61 @@ export const List = ({ newtask }) => {
         if (previousState.includes(newtask)) {
           return previousState;
         }
-        return [...previousState, [newtask, false]];
-      }
-      else{
-          return previousState;
+        return [...previousState, {task: newtask, checked: false}];
+      } else {
+        return previousState;
       }
     });
   }, [newtask]);
 
   // Event + parameter: https://stackoverflow.com/a/42597619
-  const clickhandler = (e, index) => {
-    console.log('task ' + index + ' is clicked');
-    e.currentTarget.classList.toggle("checked");
+  const newclickhandler = (e, index) => {
+    //e.currentTarget.classList.toggle("checked");
+    console.log("task " + index + " is clicked");
+    
+    // This can be done beteer. Reassigning output to a new array.
+    // Why not rendering again when using the tasks state.
+    var z = []
+
+    tasks.filter((nta, i) => {
+      if (i == index) {
+        nta.checked = !nta.checked;
+        z[i] = nta
+      }
+      else{
+        z[i] = nta
+      }
+    });
+    console.log(z);
+    console.log(tasks);
+    setTasks(z);
   };
 
-  function close(e, itemIndex) {
-    //e.currentTarget.parentNode.style.display = 'none';
-    console.log('task ' + itemIndex + ' has been closed');
-    setTasks(tasks.filter((item, index) => index !== itemIndex));
-    
+  function newclose(e, indd) {
+    console.log("task " + indd + " has been deleted");
+    setTasks(tasks.filter((ntb, i) => i !== indd));
     // To stop the code from dropping further // Ref: https://stackoverflow.com/q/50829705
     e.stopPropagation();
   }
 
+
   return (
     <ul id="list">
-      {tasks.map((task, index) => (
-        <li className = {task[1] ? 'checked' : ''} onClick={(e) => clickhandler(e, index)} key={`${task}-${index}`}>
-          {task}
+      {tasks.map((nt, index) => (
+        <li
+          className={nt.checked ? "checked" : ""}
+          onClick={(e) => newclickhandler(e, index)}
+          key={`${nt.task}-${index}`}
+        >
+          {nt.task}
           <Btn
-            onClick={(e) => close(e, index)}
+            onClick={(e) => newclose(e, index)}
             className="xButton"
             input_text={"\u00D7"}
           />
         </li>
-        
       ))}
+      {/* {tasks.map((nt) => nt.task)} */}
     </ul>
   );
 };
